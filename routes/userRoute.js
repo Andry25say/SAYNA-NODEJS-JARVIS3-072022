c5c93d15480a385e9bd09712db330b03a8ad287a
Andry <andryrichard2022@gmail.com>
2022-08-01 17:06:20 +0300

    jarvis depot finale

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,26 +1,81 @@
 const express = require('express');
 const db = require('../models/dbConfig');
-
+const { registerValidation, loginValidation } = require('../validation');
+const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 const router = express.Router();
 //default route
 router.get('/', (req, res) => {
     res.status(200).render('index');
 });
+//Api routes
+//login 
+router.post('/login', async(req, res, next)=>{
+    try{
+    const email = req.body.email;
+    const password = req.body.password;
+    user = await db.getUserByEmail(email);
+     
+    if(!user){
+        return res.json({
+            message: "Invalid email or password"
+        })
+    }
+ 
+    const mdpValid = compareSync(password, user.password);
+    if(mdpValid){
+        user.password = undefined;
+        const jsontoken = jsonwebtoken.sign({user: user}, process.env.SECRET_KEY, { expiresIn: '30m'} );
+        res.cookie('token', jsontoken, { httpOnly: true, secure: true, SameSite: 'strict' , expires: new Date(Number(new Date()) + 30*60*1000) }); 
+ 
+        res.json({token: jsontoken});
+       //return res.redirect('/mainpage') ;
+ 
+    }  else{
+        return res.json({
+            message: "Invalid email or password"
+        });
+    } 
+ 
+    } catch(err){
+        console.log(err);
+    }
+});
 
-//all users
-router.post('/login', async(req,res)=>{
-    const data = req.body;
-    db.query('INSERT INTO user SET ?',data,(err,results,fields)=>{
-        if (err) {
-            res.status(400).send(err.message);
-        }else{
-            res.status(200).send({err:false,data:results,message:'login now'})
-        }
-    })
+//register
+router.post('/register', async (req, res, next)=>{
+    try{
+        const userName = req.body.userName;
+        const email = req.body.email;
+        let password = req.body.password;
 
-}
-)
+              if (!userName || !email || !password) {
+                return res.sendStatus(400);
+             }
+  
+             const salt = genSaltSync(10);
+             password = hashSync(password, salt);
+  
+               
+  
+        const user =  await db.insertUser(userName, email, password);
+         
+        const jsontoken = jsonwebtoken.sign({user: user}, process.env.SECRET_KEY, { expiresIn: '30m'} );
+        res.cookie('token', jsontoken, { httpOnly: true, secure: true, SameSite: 'strict' , expires: new Date(Number(new Date()) + 30*60*1000) });
+ 
+ 
+        res.json({token: jsontoken});
+ 
+            //return res.redirect('/mainpage');
+  
+    } catch(err){    
+        console.log(err);
+        res.sendStatus(400);
+    }
+});
 //middlewares pour les erreurs 404
 router.use((req, res) => {
     res.status(404).render('erreur');
